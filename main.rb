5f3cf3e3b0024be60eeee083f4b0268cb028aee5
victorioc <victoriocabrera@gmail.com>
2018-10-29 12:19:10 -0400

    succesfully creating data object with all contributors and last seven review links

--- main.rb
+++ main.rb
@@ -1,22 +1,54 @@
 require 'nokogiri'
-require 'HTTParty'
 require 'open-uri'
 require 'byebug'
+require 'marshal'
+# Variable storage--put stuff here
+critics = Marshal.load('critics_data')
+byebug
+page = Nokogiri::HTML(open('https://www.rogerebert.com/contributors'))
+critic_links = []
+review_links = []
+review_bodies = []
+critics_arr = []
 
-# page = HTTParty.get('https://rogerebert.com/reviews/the-spectacular-now-2013')
-page = Nokogiri::HTML(open('https://www.rogerebert.com/contributors/roger-ebert'))
-grafs = []
-
-text_arr = page.xpath('//h5[@class="title"]/a/@href').slice(0,4)
-
-text_arr.each do |graf|
-  grafs.push graf.text
+# Populate the critics_arr with urls for each contributor
+critics_list = page.xpath('//figcaption/h4/a/@href')
+base_url = 'https://www.rogerebert.com'
+critics_list.each do |critic|
+  critic_url = base_url + critic.text
+  critics_arr.push(
+    {name:'',
+    page_link: critic_url,
+    reviews: []}
+    )
+end
+# Visit each page, grab contributor name and last X reviews
+# Doesn't yet filter for non-critic contributors
+critics_arr.each do |critic|
+  critic_page = Nokogiri::HTML(open(critic[:page_link]))
+  critic[:name] = critic_page.xpath('//h1').text
+  review_objs = critic_page.xpath('//h5/a/@href')
+  review_links = []
+  review_objs.each do |object|
+    review_links.push object.text
+  end
+  critic[:reviews].push review_links.slice(0,7)
+end
+critic_storage = Marshal.dump(critics_arr)
+File.open('critics_data', 'w+') do |f|
+  f.write critic_storage
+  f.close
+end
+review_links.each do |link|
+  review_link = 'https://www.rogerebert.com/' + link
+  review_page = Nokogiri::HTML(open(review_link))
+  review_text = review_page.xpath('//div[@itemprop]/p').text
+  review_bodies.push review_text
 end
 
-File.open('The-Spectacular-Now', 'w+') do |f|
-  f.write grafs
+File.open('review_bodies', 'w+') do |f|
+  f.write review_bodies
+  byebug
   f.close
 end
 
-
-# puts $LOAD_PATH
